8560d7341f22c93fb2d58e3ca68e548e1eeb8fae
gunerhanale <gunerhanale@gmail.com>
2021-04-19 22:19:39 +0200

    Implemented End-to-End List Employee Feature

--- src/main/java/net/supernova/springboot/service/EmployeeServiceImpl.java
+++ src/main/java/net/supernova/springboot/service/EmployeeServiceImpl.java
@@ -3,9 +3,11 @@ package net.supernova.springboot.service;
 import net.supernova.springboot.model.Employee;
 import net.supernova.springboot.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService{
 
     @Autowired
